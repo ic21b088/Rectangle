83e12bc9f6b4e28b8a7a60ee13c84607634dc141
Rajvinder Singh <ic21b088@technikum-wien.at>
2022-12-06 12:15:15 +0100

    #1 added scope calculation of rectangle

--- src/main/java/at/fhtw/Rectangle/Controller/RectangleController.java
+++ src/main/java/at/fhtw/Rectangle/Controller/RectangleController.java
@@ -1,4 +1,16 @@
 package at.fhtw.Rectangle.Controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class RectangleController {
+
+    @GetMapping("/rectangle/{width}/{height}")
+    public int calcRectangle(@PathVariable int width, @PathVariable int height)
+    {
+        return width * height;
+    }
+
 }
