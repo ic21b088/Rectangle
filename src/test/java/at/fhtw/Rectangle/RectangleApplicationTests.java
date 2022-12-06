eb21457ec54bb2f56fd0c2f35220764d05f4aff8
Rajvinder Singh <ic21b088@technikum-wien.at>
2022-12-06 13:33:37 +0100

    #2 added unittests for calculation

--- src/test/java/at/fhtw/Rectangle/RectangleApplicationTests.java
+++ src/test/java/at/fhtw/Rectangle/RectangleApplicationTests.java
@@ -1,13 +1,26 @@
 package at.fhtw.Rectangle;
 
+import at.fhtw.Rectangle.Controller.RectangleController;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @SpringBootTest
 class RectangleApplicationTests {
 
 	@Test
-	void contextLoads() {
+	void testCalculationRectangle() {
+
+		// Given
+		RectangleController controller = new RectangleController();
+
+		// When
+		int result = controller.calcRectangle(5, 12);
+
+		// Then
+		assertEquals(result, 60);
+
 	}
 
 }
