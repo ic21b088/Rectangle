64efc4d853ff75018272301825d3ba2f55d4663d
Rajvinder Singh <ic21b088@technikum-wien.at>
2022-12-06 10:18:37 +0100

    Initial commit

---
+++ src/test/java/at/fhtw/Rectangle/RectangleApplicationTests.java
@@ -0,0 +1,13 @@
+package at.fhtw.Rectangle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RectangleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
